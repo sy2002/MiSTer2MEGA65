a955aa0f6322f762d30158eba365ca774e15af78
sy2002 <code@sy2002.de>
2022-03-03 14:56:57 +0100

    bugfix: QNICE_FIRMWARE constant path updated

--- MEGA65/vhdl/mega65.vhd
+++ MEGA65/vhdl/mega65.vhd
@@ -88,7 +88,7 @@ architecture beh of MEGA65_Core is
 -- QNICE Firmware: Use the regular QNICE "operating system" called "Monitor" while developing
 -- and debugging and use the MiSTer2MEGA65 firmware in the release version
 constant QNICE_FIRMWARE       : string  := "../../QNICE/monitor/monitor.rom";
---constant QNICE_FIRMWARE       : string  := "../m2m-rom/m2m-rom.rom";
+--constant QNICE_FIRMWARE       : string  := "../../MEGA65/m2m-rom/m2m-rom.rom";
 
 -- MiSTer2MEGA65 default resolution is HDMI 720p @ 60 Hz
 constant VIDEO_MODE           : video_modes_t := C_HDMI_720p_60;
