21acc0aaf2aa0af63e1fc0155b2ea6b3f5c71253
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2023-05-21 18:03:17 +0200

    Add comment about core resolution

--- CORE/vhdl/globals.vhd
+++ CORE/vhdl/globals.vhd
@@ -54,9 +54,12 @@ constant QNICE_CLK_SPEED      : natural := 50_000_000;   -- a change here has de
 
 -- Rendering constants (in pixels)
 --    VGA_*   size of the core's target output post scandoubler
---    FONT_*  size of one OSM character
+--    If in doubt, use twice the values found in this link:
+--    https://mister-devel.github.io/MkDocs_MiSTer/advanced/nativeres/#arcade-core-default-native-resolutions
 constant VGA_DX               : natural := 720;
 constant VGA_DY               : natural := 576;
+
+--    FONT_*  size of one OSM character
 constant FONT_FILE            : string  := "../font/Anikki-16x16-m2m.rom";
 constant FONT_DX              : natural := 16;
 constant FONT_DY              : natural := 16;
