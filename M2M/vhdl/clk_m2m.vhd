df9eea59a02510bd72927b203b5b53e88601e2dc
Michael Finn Jørgensen <mfj@weibel.dk>
2023-09-27 11:17:00 +0200

    clk_m2m: Cleanup
    Using simpler components yields simpler code.
    There should be no functional difference.

--- M2M/vhdl/clk_m2m.vhd
+++ M2M/vhdl/clk_m2m.vhd
@@ -15,9 +15,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-library unisim;
-use unisim.vcomponents.all;
-
 library xpm;
 use xpm.vcomponents.all;
 
@@ -35,7 +32,7 @@ entity clk_m2m is
       hr_clk_x2_del_o : out std_logic;   -- MEGA65 HyperRAM @ 200 MHz phase delayed
       hr_rst_o        : out std_logic;   -- MEGA65 HyperRAM reset, synchronized
 
-      hdmi_clk_sel_i  : in  std_logic;   -- 0: Chose 74.25 Mhz, 1: Choose 27.00 MHz
+      hdmi_clk_sel_i  : in  std_logic;   -- 0: Choose 74.25 Mhz, 1: Choose 27.00 MHz
       tmds_clk_o      : out std_logic;   -- HDMI's 371.25 MHz pixelclock (74.25 MHz x 5) for TMDS
       hdmi_clk_o      : out std_logic;   -- HDMI's 74.25 MHz pixelclock for 720p @ 50 Hz
       hdmi_rst_o      : out std_logic;   -- HDMI's reset, synchronized
@@ -79,70 +76,41 @@ begin
    -- VCO frequency range for Artix 7 speed grade -1 : 600 MHz - 1200 MHz
    -- f_VCO = f_CLKIN * CLKFBOUT_MULT_F / DIVCLK_DIVIDE
    
-   i_clk_qnice : MMCME2_ADV
+   i_clk_qnice : PLLE2_BASE
       generic map (
          BANDWIDTH            => "OPTIMIZED",
-         CLKOUT4_CASCADE      => FALSE,
-         COMPENSATION         => "ZHOLD",
-         STARTUP_WAIT         => FALSE,
-         CLKIN1_PERIOD        => 10.0,       -- INPUT @ 100 MHz
-         REF_JITTER1          => 0.010,
-         DIVCLK_DIVIDE        => 1,
-         CLKFBOUT_MULT_F      => 12.0,       -- 1200 MHz
+         CLKFBOUT_MULT        => 12,         -- 1200 MHz
          CLKFBOUT_PHASE       => 0.000,
-         CLKFBOUT_USE_FINE_PS => FALSE,
-         CLKOUT0_DIVIDE_F     => 24.000,     -- QNICE @ 50 MHz
-         CLKOUT0_PHASE        => 0.000,
+         CLKIN1_PERIOD        => 10.0,       -- INPUT @ 100 MHz
+         CLKOUT0_DIVIDE       => 24,         -- QNICE @ 50 MHz
          CLKOUT0_DUTY_CYCLE   => 0.500,
-         CLKOUT0_USE_FINE_PS  => FALSE,
-         CLKOUT1_DIVIDE       => 12,          -- HyperRAM @ 100 MHz
-         CLKOUT1_PHASE        => 0.000,
+         CLKOUT0_PHASE        => 0.000,
+         CLKOUT1_DIVIDE       => 12,         -- HyperRAM @ 100 MHz
          CLKOUT1_DUTY_CYCLE   => 0.500,
-         CLKOUT1_USE_FINE_PS  => FALSE,
+         CLKOUT1_PHASE        => 0.000,
          CLKOUT2_DIVIDE       => 6,          -- HyperRAM @ 200 MHz
-         CLKOUT2_PHASE        => 0.000,
          CLKOUT2_DUTY_CYCLE   => 0.500,
-         CLKOUT2_USE_FINE_PS  => FALSE,
+         CLKOUT2_PHASE        => 0.000,
          CLKOUT3_DIVIDE       => 6,          -- HyperRAM @ 200 MHz phase delayed
-         CLKOUT3_PHASE        => 180.000,
          CLKOUT3_DUTY_CYCLE   => 0.500,
-         CLKOUT3_USE_FINE_PS  => FALSE,
+         CLKOUT3_PHASE        => 180.000,
          CLKOUT4_DIVIDE       => 40,         -- Audio @ 30 MHz
-         CLKOUT4_PHASE        => 0.000,
          CLKOUT4_DUTY_CYCLE   => 0.500,
-         CLKOUT4_USE_FINE_PS  => FALSE
+         CLKOUT4_PHASE        => 0.000,
+         DIVCLK_DIVIDE        => 1,
+         REF_JITTER1          => 0.010,
+         STARTUP_WAIT         => FALSE
       )
       port map (
-         -- Output clocks
+         CLKFBIN             => qnice_fb_mmcm,
          CLKFBOUT            => qnice_fb_mmcm,
+         CLKIN1              => sys_clk_i,
          CLKOUT0             => qnice_clk_mmcm,
          CLKOUT1             => hr_clk_x1_mmcm,
          CLKOUT2             => hr_clk_x2_mmcm,
          CLKOUT3             => hr_clk_x2_del_mmcm,
          CLKOUT4             => audio_clk_mmcm,
-         -- Input clock control
-         CLKFBIN             => qnice_fb_mmcm,
-         CLKIN1              => sys_clk_i,
-         CLKIN2              => '0',
-         -- Tied to always select the primary input clock
-         CLKINSEL            => '1',
-         -- Ports for dynamic reconfiguration
-         DADDR               => (others => '0'),
-         DCLK                => '0',
-         DEN                 => '0',
-         DI                  => (others => '0'),
-         DO                  => open,
-         DRDY                => open,
-         DWE                 => '0',
-         -- Ports for dynamic phase shift
-         PSCLK               => '0',
-         PSEN                => '0',
-         PSINCDEC            => '0',
-         PSDONE              => open,
-         -- Other control and status signals
          LOCKED              => qnice_locked,
-         CLKINSTOPPED        => open,
-         CLKFBSTOPPED        => open,
          PWRDWN              => '0',
          RST                 => '0'
       ); -- i_clk_qnice
@@ -151,55 +119,29 @@ begin
    -- Generate 74.25 MHz for 720p (50 Hz or 60 Hz) and 5x74.25 MHz = 371.25 MHz for TMDS
    ---------------------------------------------------------------------------------------
 
-   i_clk_hdmi_720p : MMCME2_ADV
+   i_clk_hdmi_720p : MMCME2_BASE
       generic map (
          BANDWIDTH            => "OPTIMIZED",
-         CLKOUT4_CASCADE      => FALSE,
-         COMPENSATION         => "ZHOLD",
-         STARTUP_WAIT         => FALSE,
-         CLKIN1_PERIOD        => 10.0,       -- INPUT @ 100 MHz
-         REF_JITTER1          => 0.010,
-         DIVCLK_DIVIDE        => 5,
          CLKFBOUT_MULT_F      => 37.125,     -- f_VCO = (100 MHz / 5) x 37.125 = 742.5 MHz
          CLKFBOUT_PHASE       => 0.000,
-         CLKFBOUT_USE_FINE_PS => FALSE,
-         CLKOUT0_DIVIDE_F     => 2.000,      -- 371.25 MHz
-         CLKOUT0_PHASE        => 0.000,
+         CLKIN1_PERIOD        => 10.0,       -- INPUT @ 100 MHz
+         CLKOUT0_DIVIDE_F     => 2.000,      -- TMDS @ 371.25 MHz
          CLKOUT0_DUTY_CYCLE   => 0.500,
-         CLKOUT0_USE_FINE_PS  => FALSE,
-         CLKOUT1_DIVIDE       => 10,         -- 74.25 MHz
-         CLKOUT1_PHASE        => 0.000,
+         CLKOUT0_PHASE        => 0.000,
+         CLKOUT1_DIVIDE       => 10,         -- HDMI @ 74.25 MHz
          CLKOUT1_DUTY_CYCLE   => 0.500,
-         CLKOUT1_USE_FINE_PS  => FALSE
+         CLKOUT1_PHASE        => 0.000,
+         DIVCLK_DIVIDE        => 5,
+         REF_JITTER1          => 0.010,
+         STARTUP_WAIT         => FALSE
       )
       port map (
-         -- Output clocks
+         CLKFBIN             => hdmi_720p_fb_mmcm,
          CLKFBOUT            => hdmi_720p_fb_mmcm,
+         CLKIN1              => sys_clk_i,
          CLKOUT0             => tmds_720p_clk_mmcm,
          CLKOUT1             => hdmi_720p_clk_mmcm,
-         -- Input clock control
-         CLKFBIN             => hdmi_720p_fb_mmcm,
-         CLKIN1              => sys_clk_i,
-         CLKIN2              => '0',
-         -- Tied to always select the primary input clock
-         CLKINSEL            => '1',
-         -- Ports for dynamic reconfiguration
-         DADDR               => (others => '0'),
-         DCLK                => '0',
-         DEN                 => '0',
-         DI                  => (others => '0'),
-         DO                  => open,
-         DRDY                => open,
-         DWE                 => '0',
-         -- Ports for dynamic phase shift
-         PSCLK               => '0',
-         PSEN                => '0',
-         PSINCDEC            => '0',
-         PSDONE              => open,
-         -- Other control and status signals
          LOCKED              => hdmi_720p_locked,
-         CLKINSTOPPED        => open,
-         CLKFBSTOPPED        => open,
          PWRDWN              => '0',
          RST                 => '0'
       ); -- i_clk_hdmi_720p
@@ -208,55 +150,29 @@ begin
    -- Generate 27.00 MHz for 576p @ 50 Hz and 5x27.00 MHz = 135.0 MHz for TMDS
    ---------------------------------------------------------------------------------------
 
-   i_clk_hdmi_576p : MMCME2_ADV
+   i_clk_hdmi_576p : MMCME2_BASE
       generic map (
          BANDWIDTH            => "OPTIMIZED",
-         CLKOUT4_CASCADE      => FALSE,
-         COMPENSATION         => "ZHOLD",
-         STARTUP_WAIT         => FALSE,
-         CLKIN1_PERIOD        => 10.0,       -- INPUT @ 100 MHz
-         REF_JITTER1          => 0.010,
-         DIVCLK_DIVIDE        => 5,
          CLKFBOUT_MULT_F      => 47.250,     -- f_VCO = (100 MHz / 5) x 47.250 = 945 MHz
          CLKFBOUT_PHASE       => 0.000,
-         CLKFBOUT_USE_FINE_PS => FALSE,
-         CLKOUT0_DIVIDE_F     => 7.000,      -- 135.0 MHz
-         CLKOUT0_PHASE        => 0.000,
+         CLKIN1_PERIOD        => 10.0,       -- INPUT @ 100 MHz
+         CLKOUT0_DIVIDE_F     => 7.000,      -- TMDS @ 135.0 MHz
          CLKOUT0_DUTY_CYCLE   => 0.500,
-         CLKOUT0_USE_FINE_PS  => FALSE,
-         CLKOUT1_DIVIDE       => 35,         -- 27.00 MHz
-         CLKOUT1_PHASE        => 0.000,
+         CLKOUT0_PHASE        => 0.000,
+         CLKOUT1_DIVIDE       => 35,         -- HDMI @ 27.00 MHz
          CLKOUT1_DUTY_CYCLE   => 0.500,
-         CLKOUT1_USE_FINE_PS  => FALSE
+         CLKOUT1_PHASE        => 0.000,
+         DIVCLK_DIVIDE        => 5,
+         REF_JITTER1          => 0.010,
+         STARTUP_WAIT         => FALSE
       )
       port map (
-         -- Output clocks
+         CLKFBIN             => hdmi_576p_fb_mmcm,
          CLKFBOUT            => hdmi_576p_fb_mmcm,
+         CLKIN1              => sys_clk_i,
          CLKOUT0             => tmds_576p_clk_mmcm,
          CLKOUT1             => hdmi_576p_clk_mmcm,
-         -- Input clock control
-         CLKFBIN             => hdmi_576p_fb_mmcm,
-         CLKIN1              => sys_clk_i,
-         CLKIN2              => '0',
-         -- Tied to always select the primary input clock
-         CLKINSEL            => '1',
-         -- Ports for dynamic reconfiguration
-         DADDR               => (others => '0'),
-         DCLK                => '0',
-         DEN                 => '0',
-         DI                  => (others => '0'),
-         DO                  => open,
-         DRDY                => open,
-         DWE                 => '0',
-         -- Ports for dynamic phase shift
-         PSCLK               => '0',
-         PSEN                => '0',
-         PSINCDEC            => '0',
-         PSDONE              => open,
-         -- Other control and status signals
          LOCKED              => hdmi_576p_locked,
-         CLKINSTOPPED        => open,
-         CLKFBSTOPPED        => open,
          PWRDWN              => '0',
          RST                 => '0'
       ); -- i_clk_hdmi_576p
@@ -300,7 +216,7 @@ begin
          S  => hdmi_clk_sel_i,
          I0 => tmds_720p_clk_mmcm,
          I1 => tmds_576p_clk_mmcm,
-         O => tmds_clk_o
+         O  => tmds_clk_o
       );
 
    hdmi_clk_bufgmux : BUFGMUX_CTRL
@@ -308,7 +224,7 @@ begin
          S  => hdmi_clk_sel_i,
          I0 => hdmi_720p_clk_mmcm,
          I1 => hdmi_576p_clk_mmcm,
-         O => hdmi_clk_o
+         O  => hdmi_clk_o
       );
 
    -------------------------------------
@@ -335,7 +251,7 @@ begin
          -- 1-bit input: Source reset signal
          -- Important: The HyperRAM needs to be reset when ascal is being reset! The Avalon memory interface
          -- assumes that both ends maintain state information and agree on this state information. Therefore,
-         -- one side can not be reset in the middle of e.g. a burst transaction, without the other end becoming confused.         
+         -- one side can not be reset in the middle of e.g. a burst transaction, without the other end becoming confused.
          src_arst  => not (qnice_locked and sys_rstn_i and core_rstn_i) or hdmi_rst_o,
          dest_clk  => hr_clk_x1_o,      -- 1-bit input: Destination clock.
          dest_arst => hr_rst_o          -- 1-bit output: src_rst synchronized to the destination clock domain.
@@ -363,7 +279,7 @@ begin
          src_arst  => not (hdmi_720p_locked and hdmi_576p_locked and sys_rstn_i),   -- 1-bit input: Source reset signal.
          dest_clk  => hdmi_clk_o,       -- 1-bit input: Destination clock.
          dest_arst => hdmi_rst_o        -- 1-bit output: src_rst synchronized to the destination clock domain.
-                                       -- This output is registered.
+                                        -- This output is registered.
       );
 
    p_sys_pps : process (sys_clk_i)
